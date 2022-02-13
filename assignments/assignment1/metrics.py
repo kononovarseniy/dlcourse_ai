8311f1864b4431141fa8e7614f9387dbee9699db
Arseniy Kononov <kononovarseniy@gmail.com>
2022-02-13 11:16:31 +0700

    Task 1.1

--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def binary_classification_metrics(prediction, ground_truth):
     '''
     Computes metrics for binary classification
@@ -9,10 +11,14 @@ def binary_classification_metrics(prediction, ground_truth):
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
+    fp = np.count_nonzero((prediction == True) & (ground_truth == False))
+    tp = np.count_nonzero((prediction == True) & (ground_truth == True))
+    fn = np.count_nonzero((prediction == False) & (ground_truth == True))
+    tn = np.count_nonzero((prediction == False) & (ground_truth == False))
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    accuracy = (tp + tn) / (fp + tp + fn + tn)
+    f1 = 2 / (1 / recall + 1 / precision)
 
     # TODO: implement metrics!
     # Some helpful links:
@@ -34,4 +40,4 @@ def multiclass_accuracy(prediction, ground_truth):
     accuracy - ratio of accurate predictions to total samples
     '''
     # TODO: Implement computing accuracy
-    return 0
+    return np.count_nonzero(prediction == ground_truth) / prediction.shape[0]
